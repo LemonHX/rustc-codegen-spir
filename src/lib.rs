9d17d735dd64c381406ce1c2433156b541c961bd
LemonHX <lemonhx@lemonhx.moe>
2024-03-26 11:13:51 +0800

    set up rustc dev env

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#![feature(rustc_private)]
+
+extern crate rustc_apfloat;
+extern crate rustc_arena;
+extern crate rustc_ast;
+extern crate rustc_attr;
+extern crate rustc_codegen_ssa;
+extern crate rustc_data_structures;
+extern crate rustc_driver;
+extern crate rustc_errors;
+extern crate rustc_hir;
+extern crate rustc_index;
+extern crate rustc_interface;
+extern crate rustc_metadata;
+extern crate rustc_middle;
+extern crate rustc_session;
+extern crate rustc_span;
+extern crate rustc_target;
+
